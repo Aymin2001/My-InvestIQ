6909ec1d4d2f18e1496b46dcbd0295d34ae0762a
Aymin2001 <114797975+Aymin2001@users.noreply.github.com>
2024-03-25 09:49:11 -0700

    addede delate bckend

--- backend/routes/Contact.js
+++ backend/routes/Contact.js
@@ -53,6 +53,23 @@ router.get('/getContacts', async (req, res) => {
   }
 });
 
+// Route to delete a specific contact by ID
+router.delete('/contact/:id', async (req, res) => {
+  try {
+    const contactId = req.params.id;
+    
+    // Find the contact by ID and delete it
+    const deletedContact = await Contact.findByIdAndDelete(contactId);
 
+    if (!deletedContact) {
+      return res.status(404).json({ error: 'Contact not found' });
+    }
+
+    res.status(200).json({ message: 'Contact deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting contact:', error);
+    res.status(500).json({ error: 'An error occurred while deleting contact' });
+  }
+});
 
 module.exports = router;
