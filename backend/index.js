b41d6f1ec984ae2bc6ed992f915ebb9a9b71a8f3
Aymin2001 <114797975+Aymin2001@users.noreply.github.com>
2024-03-25 04:35:38 -0700

    added cors policy

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+// Allow requests from your frontend domain
+const allowedOrigins = ['https://my-invest-iq-frontend.vercel.app/', 'https://my-invest-iq-backend.vercel.app/']; // Add your frontend domain(s) here
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
+
 app.get("/check",(req,res) =>{
     res.json('hello');
 })
